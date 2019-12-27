17e735ee4cf42168fe9a2c7edc2b0be1de24ec19
chenkl <424534159@qq.com>
2019-12-27 18:22:52 +0800

    add test_flash_program project

---
+++ test/test_program_flash/program_flash/program_flash/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace program_flash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.select = new System.Windows.Forms.Button();
+            this.startprogram = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.logbox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.file_path_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // select
+            // 
+            this.select.Location = new System.Drawing.Point(475, 19);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(75, 23);
+            this.select.TabIndex = 0;
+            this.select.Text = "选择";
+            this.select.UseVisualStyleBackColor = true;
+            this.select.Click += new System.EventHandler(this.select_Click);
+            // 
+            // startprogram
+            // 
+            this.startprogram.Location = new System.Drawing.Point(12, 196);
+            this.startprogram.Name = "startprogram";
+            this.startprogram.Size = new System.Drawing.Size(64, 43);
+            this.startprogram.TabIndex = 1;
+            this.startprogram.Text = "开始";
+            this.startprogram.UseVisualStyleBackColor = true;
+            this.startprogram.Click += new System.EventHandler(this.startprogram_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(82, 210);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(468, 20);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // logbox
+            // 
+            this.logbox.Location = new System.Drawing.Point(12, 56);
+            this.logbox.Multiline = true;
+            this.logbox.Name = "logbox";
+            this.logbox.Size = new System.Drawing.Size(538, 134);
+            this.logbox.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // file_path_box
+            // 
+            this.file_path_box.Location = new System.Drawing.Point(12, 19);
+            this.file_path_box.Name = "file_path_box";
+            this.file_path_box.Size = new System.Drawing.Size(448, 21);
+            this.file_path_box.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 256);
+            this.Controls.Add(this.file_path_box);
+            this.Controls.Add(this.logbox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.startprogram);
+            this.Controls.Add(this.select);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button select;
+        private System.Windows.Forms.Button startprogram;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox logbox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox file_path_box;
+    }
+}
+
---
+++ test/test_program_flash/program_flash/program_flash/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace program_flash
+{
+    public partial class Form1 : Form
+    {
+        Process p;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void select_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "固件|*.hex";
+            openFileDialog1.InitialDirectory = "C:\\Users\\Administrator\\Desktop";
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                file_path_box.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void flashProgram()
+        {
+            if (p != null)
+            {
+                p.Close();
+            }
+            //创建一个进程
+            p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;//是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+            p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+            p.StartInfo.WorkingDirectory = @"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility";
+
+            string strCMD = "";
+            strCMD = @"ST-LINK_CLI.exe -c SWD UR -P ";
+            strCMD = strCMD + "\"";
+            strCMD = strCMD + file_path_box.Text;
+            strCMD = strCMD + "\"";
+            strCMD = strCMD + " -V";
+
+            p.StartInfo.Arguments = "/c " + strCMD;    //设定程式执行参数
+
+            p.Start();//启动程序
+            p.BeginOutputReadLine();// 异步获取命令行内容
+        }
+        private void loop()
+        {
+            for (var i = 0; i <= 100; i++)
+            {
+                progressBar1.Value = i;
+                Task.Delay(250);
+            }
+        }
+
+        private void startprogram_Click(object sender, EventArgs e)
+        {
+            flashProgram();
+            //loop();
+        }
+    }
+}
